35b8264a8b1d74909ce15fbb6b36e255ec4325a2
vinnBoy <42758140+vinnBoy@users.noreply.github.com>
2018-11-17 16:32:11 -0200

    Revert "Eigth Commit"
    This reverts commit 5868baabab6d733415ae71f8f44af4e0ff68106b.

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-// $route['pages/upload_file'] = 'pages/upload_file';
+
 $route['default_controller'] = 'users/login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
