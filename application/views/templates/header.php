3462b2621da77e8867eb2c1e1c34c9db1001d2db
vinnBoy <42758140+vinnBoy@users.noreply.github.com>
2018-11-17 16:37:27 -0200

    Ninth Commit

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,7 +6,7 @@
     </head>
     <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a class="navbar-brand" href="<?php echo base_url();?>">StarriAD</a>
+    <a class="navbar-brand" href="<?php echo base_url();?>pages/admin">StarriAD</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
