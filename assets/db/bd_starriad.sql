5b6b1d1cbbeccc052c012955f20fc7162a14027c
vinnBoy <42758140+vinnBoy@users.noreply.github.com>
2018-12-19 07:09:50 -0200

    Update bd_starriad.sql

--- assets/db/bd_starriad.sql
+++ assets/db/bd_starriad.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.8.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 18-Dez-2018 às 20:26
--- Versão do servidor: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Generation Time: 19-Dez-2018 às 10:09
+-- Versão do servidor: 10.1.31-MariaDB
+-- PHP Version: 7.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -31,7 +33,7 @@ CREATE TABLE `campanhas` (
   `titulo` varchar(255) NOT NULL,
   `descricao` varchar(255) NOT NULL,
   `nome_arquivo` varchar(255) NOT NULL,
-  `email` varchar(15) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `data_inicio` date NOT NULL,
   `data_encerramento` date NOT NULL,
   `investimento` double NOT NULL,
@@ -48,7 +50,8 @@ CREATE TABLE `campanhas` (
 --
 
 INSERT INTO `campanhas` (`id`, `titulo`, `descricao`, `nome_arquivo`, `email`, `data_inicio`, `data_encerramento`, `investimento`, `valor_desconto`, `num_cupons`, `categoria`, `sub_categoria`, `palavras_chave`, `pergunta`) VALUES
-(1, 'Russia in 15 seconds', 'asdasd', 'Russia in_15_seconds.MP4', 'admin@admin.com', '0000-00-00', '0000-00-00', 0, 0, 0, '', '', '', '');
+(1, 'Russia in 15 seconds', 'asdasd', 'Russia in_15_seconds.MP4', 'admin@admin.com', '0000-00-00', '0000-00-00', 0, 0, 0, 'asdsad', '', '', ''),
+(7, 'asd', 's', 'Saving you $60+ in 15 seconds.MP4', 'vinicius.rmoraes@hotmail.com', '0000-00-00', '0000-00-00', 123, 123, 12, 'asd', 'asd', 'asd', 'asd');
 
 -- --------------------------------------------------------
 
@@ -57,6 +60,7 @@ INSERT INTO `campanhas` (`id`, `titulo`, `descricao`, `nome_arquivo`, `email`, `
 --
 
 CREATE TABLE `filiais` (
+  `id` int(255) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `cep` int(255) NOT NULL,
   `rua` varchar(255) NOT NULL,
@@ -65,15 +69,17 @@ CREATE TABLE `filiais` (
   `bairro` varchar(255) NOT NULL,
   `cidade` varchar(255) NOT NULL,
   `estado` varchar(255) NOT NULL,
-  `centro_comercial` varchar(255) NOT NULL
+  `centro_comercial` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `filiais`
 --
 
-INSERT INTO `filiais` (`nome`, `cep`, `rua`, `numero`, `complemento`, `bairro`, `cidade`, `estado`, `centro_comercial`) VALUES
-('sadas', 123, 'sda', 213, 'sad', 'asd', 'asd', 'asd', 'asd');
+INSERT INTO `filiais` (`id`, `nome`, `cep`, `rua`, `numero`, `complemento`, `bairro`, `cidade`, `estado`, `centro_comercial`, `email`) VALUES
+(1, 'sadas', 123, 'sda', 213, 'sad', 'asd', 'asd', 'asd', 'asd', 'admin@admin.com'),
+(2, 'asd', 21251, 'asddas', 21, 'asd', 'asd', 'asd', 'asd', 'asd', 'admin@admin.com');
 
 -- --------------------------------------------------------
 
@@ -109,8 +115,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `nome`, `nome_empresa`, `email`, `telefone`, `razao_social`, `cnpj`, `banco`, `agencia`, `conta`, `cep`, `rua`, `numero`, `complemento`, `bairro`, `cidade`, `estado`, `senha`, `termos`, `admin`) VALUES
-(1, 'User1', 'EmpresaNome1', 'nome@empresa.com', '1188884444', '', '', '', '', '', '', '', '', '', '', '', '', '81dc9bdb52d04dc20036dbd8313ed055', 0, 0),
-(3, 'Admin', 'admin', 'admin@admin.com', '0022334455', 'asdas', '4242', '5454', '515', '216', '08021540', 'Rua Alfredo Albertini', '1651', 'asd', 'Jardim São Vicente', 'São Paulo', 'SP', '21232f297a57a5a743894a0e4a801fc3', 1, 1);
+(1, 'User1', 'EmpresaNome1', 'nome@empresa.com', '1188884444', 'asds', '213', '123', '123', '123', '08021540', 'Rua Alfredo Albertini', '21', 'asd', 'Jardim São Vicente', 'São Paulo', 'SP', '81dc9bdb52d04dc20036dbd8313ed055', 1, 0),
+(3, 'Admin', 'admin', 'admin@admin.com', '0022334455', 'asdas', '4242', '5454', '515', '216', '08021540', 'Rua Alfredo Albertini', '1651', 'asd', 'Jardim São Vicente', 'São Paulo', 'SP', '21232f297a57a5a743894a0e4a801fc3', 1, 1),
+(5, 'Vinicius', 'Null', 'vinicius.rmoraes@hotmail.com', '1122222222', 'asds', '213', '123', '123', '123', '08021540', 'Rua Alfredo Albertini', '21', 'asd', 'Jardim São Vicente', 'São Paulo', 'SP', '827ccb0eea8a706c4c34a16891f84e7b', 1, 0);
 
 --
 -- Indexes for dumped tables
@@ -120,6 +127,12 @@ INSERT INTO `users` (`id`, `nome`, `nome_empresa`, `email`, `telefone`, `razao_s
 -- Indexes for table `campanhas`
 --
 ALTER TABLE `campanhas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `filiais`
+--
+ALTER TABLE `filiais`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -136,12 +149,21 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `campanhas`
 --
 ALTER TABLE `campanhas`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `filiais`
+--
+ALTER TABLE `filiais`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
