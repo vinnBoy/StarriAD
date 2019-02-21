464247f7c1374e2e0b1f276b323e6f6ff80c4404
victoroshiro <victor.za.oshiro5@gmail.com>
2019-02-21 20:14:40 -0300

    ajustes

--- application/views/pages/campanhas.php
+++ application/views/pages/campanhas.php
@@ -11,13 +11,13 @@
 
 <div class="container table-active mt-5">
 <h3 class="text-center center-x ">Listagem de Campanhas</h3>
-  <div class="row pt-5" >  
+  <div class="row pt-5" >
 
     <?php foreach($campanhas as $campanha) : ?>
       <?php $thumb_name = explode('.',$campanha['nome_arquivo']); ?>
       <div class="col-md-4 padding-left-10" >
         <h5><?php echo $campanha['titulo']; ?></h5>
-        <label ><?php echo $campanha['descricao']; ?></label> 
+        <label ><?php echo $campanha['descricao']; ?></label>
 
                 <img width="auto" height="240" src="<?php echo base_url() .'uploads/'.$thumb_name['0'].".jpg"; ?>"><br><br>
             <label ><?php echo "Categorias: ".$campanha['categoria']."|".$campanha['sub_categoria']; ?></label><br>
@@ -30,20 +30,20 @@
             $myFormatForView = date("m/d/Y ", $time);
             ?>
             <label ><?php echo "Data de encerramento: ".$myFormatForView; ?></label> <br>
-          <a href="<?= base_url('pages/cupons?id='.$campanha['id'])?>">
+          <a href="<?= base_url('pages/cuponsCamp?id='.$campanha['id'])?>">
               <button>Cupons</button>
           </a>
           <br>
-        
+
         <?php echo form_open('pages/editarCampanha?id='.$campanha['id']); ?>
           <input type="submit" value="Editar" class="btn btn-danger btn-sm">
           <input type="hidden" name="nome_arquivo" value="<?php echo $campanha['nome_arquivo']; ?>">
-        </form> <br><br> 
+        </form> <br><br>
       </div>
 
     <?php endforeach; ?>
 
-    </div>    
+    </div>
   </div>
 
   </div>
